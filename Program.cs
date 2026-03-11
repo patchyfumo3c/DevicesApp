33db42fc344e9bbaf9159165f349a9a29bdda3e8
md <mattyoott@gmail.com>
2026-03-12 01:59:10 +1300

    Added a new function for calculation, added a few comments, cleaned up variable declarations

--- Program.cs
+++ Program.cs
@@ -5,27 +5,21 @@ namespace DevicesApp
     internal class Program
     {
         //Global Variables
-        static string output = "";
-        static decimal totalInsurance = 0;
-        static int laptopCounter = 0;
-        static int desktopCounter = 0;
-        static int otherDeviceCounter = 0;
-        
-        static decimal mostValuableDeviceCost = 0;
-        static string mostValuableDevice = "";
+        static string output = "",  mostValuableDevice = "";
+        static decimal totalInsurance = 0, mostValuableDeviceCost = 0,  totalCost = 0;
+        static int laptopCounter = 0, desktopCounter = 0, otherDeviceCounter = 0;
 
         static readonly List<string> STEPS = new List<string> { "Enter the device name", "\nEnter the device Category\n1:   Laptop\n2:   Desktop\n3:   Other", "\nEnter the cost", "Enter the amount of these devices" };
         static readonly List<string> ERRORS = new List<string> { "Please enter four or more characters", "Please enter a value of 1, 2 or 3.", "Please enter a value between 1 and 50000", "Please enter a value between 1 and 5000" };
+        
 
 
-
-
+        //Main method
         static void Main(string[] args)
         {
             //Local variables
             string loop = "";
-            decimal totalCost = 0;
-
+            decimal totalDeviceCost;
 
             //Display title page
             Console.WriteLine("  _____             _                            \r\n |  __ \\           (_)                           \r\n | |  | | _____   ___  ___ ___                   \r\n | |  | |/ _ \\ \\ / / |/ __/ _ \\                  \r\n | |__| |  __/\\ V /| | (_|  __/                  \r\n |_____/ \\___| \\_/ |_|\\___\\___|                  \r\n |_   _|                                         \r\n   | |  _ __  ___ _   _ _ __ __ _ _ __   ___ ___ \r\n   | | | '_ \\/ __| | | | '__/ _` | '_ \\ / __/ _ \\\r\n  _| |_| | | \\__ \\ |_| | | | (_| | | | | (_|  __/\r\n |_____|_| |_|___/\\__,_|_|  \\__,_|_| |_|\\___\\___|\r\n                                                 \r\n                                                 ");
@@ -38,9 +32,11 @@ namespace DevicesApp
             //Add Devices
             while (loop != "q")
             {
-                decimal totalDeviceCost = 0;
-                totalCost += AddDevice(totalDeviceCost);
-                
+                totalDeviceCost = 0;
+
+                //Add a single device to the toal cost
+                //totalCost += AddDevice(totalDeviceCost);
+                AddDevice(totalDeviceCost,totalCost);
                 Console.WriteLine("\n\nEnter    Add another device\nq        Quit and print summary");
                 Console.ForegroundColor = ConsoleColor.White;
                 loop = Console.ReadLine();
@@ -54,7 +50,7 @@ namespace DevicesApp
             //Generate insurance summary
             Console.Clear();
             Console.WriteLine(output);
-            Console.WriteLine($"Amount of Laptops:   {laptopCounter}\nAmount of Desktops:   {desktopCounter}\nAmount of other devices:   {otherDeviceCounter}\n\n" +
+            Console.WriteLine($"------------Summary------------\nAmount of Laptops:   {laptopCounter}\nAmount of Desktops:   {desktopCounter}\nAmount of other devices:   {otherDeviceCounter}\n\n" +
                 $"Total cost:   {totalCost:C}\nInsurance value:   {totalInsurance:C}\n" +
                 $"Most expensive device is the {mostValuableDevice} at {mostValuableDeviceCost:C}");
             Console.ReadLine();
@@ -66,19 +62,13 @@ namespace DevicesApp
 
 
         //Add one device
-        static decimal AddDevice(decimal totalDeviceCost)
+        static string AddDevice(decimal totalDeviceCost, decimal totalCost)
         {
             //Local variables
-            const decimal INSURANCEDISCOUNT = 0.1m;
-            const int DISCOUNTREQUIREMENT = 5;
-            const decimal VALUELOSSRATE = 0.95m;
-
-            string deviceName = "";
-            decimal deviceCost = 0;
+            
+            string deviceName = "", deviceCategory = "";
             int deviceAmount = 0;
-            string deviceCategory = "";
-
-
+            decimal deviceCost = 0;
 
             //Add device name
             while (deviceName.Length < 4)
@@ -110,6 +100,7 @@ namespace DevicesApp
                 }
  
             }
+
             switch (deviceCategory)
             {
                 case "1":
@@ -138,7 +129,8 @@ namespace DevicesApp
                     deviceCost = decimal.Parse(Console.ReadLine());
                     Console.ResetColor();
                 }
-                catch (Exception){}
+                catch (Exception){
+                }
                 
                 if (deviceCost < 1 || deviceCost > 50000)
                 {
@@ -159,7 +151,9 @@ namespace DevicesApp
                     deviceAmount = int.Parse(Console.ReadLine());
                     Console.ResetColor();
                 }
-                catch (Exception){}
+                catch (Exception){
+                }
+
                 if (deviceAmount < 1 || deviceAmount > 5000)
                 {
                     Console.ResetColor();
@@ -167,13 +161,30 @@ namespace DevicesApp
                 }
             }
             
+                
+            
+            //Calculate cost
+            Calculate(totalDeviceCost, deviceCost, deviceAmount, deviceName, deviceCategory);
+            return "Device added";
+        }
+
+
+
+
+
+
+        static void Calculate(decimal totalDeviceCost, decimal deviceCost, int deviceAmount, string deviceName, string deviceCategory)
+        {
+            const decimal INSURANCEDISCOUNT = 0.1m, VALUELOSSRATE = 0.95m;
+            const int DISCOUNTREQUIREMENT = 5;
 
 
             //Calculate cost
             totalDeviceCost = deviceCost * deviceAmount;
-
             if (deviceAmount > DISCOUNTREQUIREMENT)
             {
+                //If there is over 5 devices, apply the discount
+                //total cost of all devices - how many devices the insurance discount applies to * the amount saved of a device with the insurance discount.
                 totalDeviceCost -= (deviceAmount - DISCOUNTREQUIREMENT) * (deviceCost * INSURANCEDISCOUNT);
             }
 
@@ -185,6 +196,7 @@ namespace DevicesApp
             }
 
 
+
             //Add devices insurance to the total insurance
             totalInsurance += deviceCost * deviceAmount - totalDeviceCost;
 
@@ -193,29 +205,18 @@ namespace DevicesApp
             //Save results to the output
             output += $"\nTotal cost of {deviceAmount} {deviceName} devices:   {totalDeviceCost:C}\n";
 
-
             //Calculate 6 month value loss
             output += "Month    Value\n";
             for (int month = 1; month <= 6; month++)
             {
-               
-                output += $"{month}        {deviceCost:c}\n";    
+
+                output += $"{month}        {deviceCost:c}\n";
                 deviceCost = deviceCost * VALUELOSSRATE;
 
             }
-
             output += $"Device Category: {deviceCategory}\n\n";
 
-
-            //Return the total pricing of this device
-            return totalDeviceCost;
+            totalCost += totalDeviceCost;
         }
-
-
-        
-
-
-
-
     }
 }
