ce273283cfb0b15e167fa9f4709fd8f1ea932b58
md <mattyoott@gmail.com>
2026-03-18 06:39:16 +1300

    Program now stores device names and costs in a list to show storing data in a list Rewrote the boundary/invalid data checks

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ namespace DevicesApp
         //Global Variables
         static string mostValuableDevice = "";
         static decimal totalInsurance = 0, mostValuableDeviceCost = 0,  totalCost = 0;
-        static int laptopCounter = 0, desktopCounter = 0, otherDeviceCounter = 0;
+        static int laptopCounter = 0, desktopCounter = 0, otherDeviceCounter = 0, deviceCount = 0;
 
         static readonly List<string> STEPS = new List<string> { "Enter the device name", "\nEnter the device Category\n1:   Laptop\n2:   Desktop\n3:   Other", "\nEnter the cost", "Enter the amount of these devices" };
         static readonly List<string> ERRORS = new List<string> { "Please enter four or more characters", "Please enter a value of 1, 2 or 3.", "Please enter a value between 1 and 50000", "Please enter a value between 1 and 5000" };
-        
+        static List<string> deviceNames = new List<string> {""};
+        static List<decimal> deviceCosts = new List<decimal> {1};
 
 
         //Main method
@@ -35,6 +36,7 @@ namespace DevicesApp
                 totalDeviceCost = 0;
 
                 //Add the output of a single device to the end output
+                deviceCount++;
                 output += AddDevice(totalDeviceCost);
                 
                 Console.WriteLine("\n\nEnter    Add another device\nq        Quit and print summary");
@@ -66,41 +68,50 @@ namespace DevicesApp
         {
             //Local variables
             
-            string deviceName = "", deviceCategory = "", deviceSummary = "";
+            string deviceCategory = "", deviceSummary = "";
             int deviceAmount = 0;
-            decimal deviceCost = 0;
+          
             const decimal VALUELOSSRATE = 0.95m;
 
 
             //Add device name
-            while (deviceName.Length < 4)
+            Console.WriteLine($"Device {deviceCount}\n");
+            Console.WriteLine(STEPS[0]);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            deviceNames.Add(Console.ReadLine());
+            Console.ResetColor();
+
+            while (deviceNames[deviceCount].Length < 4)
             {
+                Console.WriteLine(ERRORS[0]);
                 Console.WriteLine(STEPS[0]);
+
                 Console.ForegroundColor = ConsoleColor.White;
-                deviceName = Console.ReadLine();
+                deviceNames[deviceCount] = Console.ReadLine();
                 Console.ResetColor();
 
-                if (deviceName.Length < 4)
-                {
-                    Console.WriteLine(ERRORS[0]);
-                }
             }
-            
+
+
 
 
             //Add the device to a category
+            Console.WriteLine(STEPS[1]);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            deviceCategory = Console.ReadLine();
+            Console.ResetColor();
+
             while (deviceCategory != "1" && deviceCategory != "2" && deviceCategory != "3")
             {
+                Console.WriteLine(ERRORS[1]);
                 Console.WriteLine(STEPS[1]);
+
                 Console.ForegroundColor = ConsoleColor.White;
                 deviceCategory = Console.ReadLine();
                 Console.ResetColor();
-
-                if (deviceCategory != "1" && deviceCategory != "2" && deviceCategory != "3")
-                {
-                    Console.WriteLine(ERRORS[1]);                   
-                }
- 
+                
             }
 
             switch (deviceCategory)
@@ -122,30 +133,49 @@ namespace DevicesApp
 
 
             //Add a cost to the device
-            while (deviceCost < 1 || deviceCost > 50000)
+            Console.WriteLine(STEPS[2]);
+
+            try
             {
+                Console.ForegroundColor = ConsoleColor.White;
+                deviceCosts.Add(decimal.Parse(Console.ReadLine()));
+                Console.ResetColor();
+            }
+            catch (Exception)
+            {
+                deviceCosts.Add(0);
+            }
+            while (deviceCosts[deviceCount] < 1 || deviceCosts[deviceCount] > 50000)
+            {
+                Console.ResetColor();
+                Console.WriteLine(ERRORS[2]);
                 Console.WriteLine(STEPS[2]);
+
                 try
                 {
                     Console.ForegroundColor = ConsoleColor.White;
-                    deviceCost = decimal.Parse(Console.ReadLine());
+                    deviceCosts[deviceCount] = decimal.Parse(Console.ReadLine());
                     Console.ResetColor();
                 }
-                catch (Exception){
-                }
-                
-                if (deviceCost < 1 || deviceCost > 50000)
-                {
-                    Console.ResetColor();
-                    Console.WriteLine(ERRORS[2]);
-                }
+                catch (Exception){}
             }
-            
-            
+
+
+
 
             //Add the amount of devices
+            Console.WriteLine(STEPS[3]);
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                deviceAmount = int.Parse(Console.ReadLine());
+                Console.ResetColor();
+            }
+            catch (Exception){}
             while (deviceAmount < 1 || deviceAmount > 5000)
             {
+                Console.ResetColor();
+                Console.WriteLine(ERRORS[3]);
                 Console.WriteLine(STEPS[3]);
                 try
                 {
@@ -153,34 +183,28 @@ namespace DevicesApp
                     deviceAmount = int.Parse(Console.ReadLine());
                     Console.ResetColor();
                 }
-                catch (Exception){
-                }
-
-                if (deviceAmount < 1 || deviceAmount > 5000)
-                {
-                    Console.ResetColor();
-                    Console.WriteLine(ERRORS[3]);
-                }
+                catch (Exception){}
             }
-            
-                
-            
+
+
+
+
             //Calculate cost
-            totalDeviceCost += Calculate(totalDeviceCost, deviceCost, deviceAmount, deviceName);
+            totalDeviceCost += Calculate(totalDeviceCost, deviceAmount);
 
             //Add to total cost
             totalCost += totalDeviceCost;
 
             //Save results to the output
-            deviceSummary += $"\nTotal cost of {deviceAmount} {deviceName} devices:   {totalDeviceCost:C}\n";
+            deviceSummary += $"\nTotal cost of {deviceAmount} {deviceNames[deviceCount]} devices:   {totalDeviceCost:C}\n";
 
             //Calculate 6 month value loss
             deviceSummary += "Month    Value\n";
             for (int month = 1; month <= 6; month++)
             {
 
-                deviceSummary += $"{month}        {deviceCost:c}\n";
-                deviceCost = deviceCost * VALUELOSSRATE;
+                deviceSummary += $"{month}        {deviceCosts[deviceCount]:c}\n";
+                deviceCosts[deviceCount] = deviceCosts[deviceCount] * VALUELOSSRATE;
 
             }
             deviceSummary += $"Device Category: {deviceCategory}\n\n";
@@ -196,32 +220,33 @@ namespace DevicesApp
 
 
 
-        static decimal Calculate(decimal totalDeviceCost, decimal deviceCost, int deviceAmount, string deviceName)
+        static decimal Calculate(decimal totalDeviceCost,  int deviceAmount)
         {
+            //Local variables
             const decimal INSURANCEDISCOUNT = 0.1m;
             const int DISCOUNTREQUIREMENT = 5;
 
 
             //Calculate cost
-            totalDeviceCost = deviceCost * deviceAmount;
+            totalDeviceCost = deviceCosts[deviceCount] * deviceAmount;
             if (deviceAmount > DISCOUNTREQUIREMENT)
             {
                 //If there is over 5 devices, apply the discount
                 //total cost of all devices - how many devices the insurance discount applies to * the amount saved of a device with the insurance discount.
-                totalDeviceCost -= (deviceAmount - DISCOUNTREQUIREMENT) * (deviceCost * INSURANCEDISCOUNT);
+                totalDeviceCost -= (deviceAmount - DISCOUNTREQUIREMENT) * (deviceCosts[deviceCount] * INSURANCEDISCOUNT);
             }
 
             //Compare with the current most valuable device
             if (totalDeviceCost > mostValuableDeviceCost)
             {
                 mostValuableDeviceCost = totalDeviceCost;
-                mostValuableDevice = deviceName;
+                mostValuableDevice = deviceNames[deviceCount];
             }
 
 
 
             //Add devices insurance to the total insurance
-            totalInsurance += deviceCost * deviceAmount - totalDeviceCost;
+            totalInsurance += deviceCosts[deviceCount] * deviceAmount - totalDeviceCost;
 
             
             return totalDeviceCost;
